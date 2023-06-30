4e1fe106be4a0893d4149064a3b35f2174a11f34
motionthunder <motionthunder@gmail.com>
2023-06-30 23:35:09 +0300

    update (11:34. 30/06/23)

--- myTesting/shapeComp.jsx
+++ myTesting/shapeComp.jsx
@@ -24,17 +24,51 @@ if (!project) {
                 // Сохраняем исходное положение слоя
                 var originalPos = layer.position.value;
 
-                // Создаем новую прекомпозицию с уникальным именем и размерами исходного слоя
-                var precomp = comp.layers.precompose([layer.index], layer.name + "_precomp", true);
+                // Вычисляем максимальные размеры слоя на протяжении всего времени анимации
+                var maxWidth = 0;
+                var maxHeight = 0;
+                var minX = Infinity;
+                var minY = Infinity;
+                var maxX = -Infinity;
+                var maxY = -Infinity;
+                for (var t = layer.inPoint; t <= layer.outPoint; t += comp.frameDuration) {
+                    var rect = layer.sourceRectAtTime(t, false);
+                    var pos = layer.position.valueAtTime(t, false);
+                    minX = Math.min(minX, pos[0] - rect.width / 2);
+                    minY = Math.min(minY, pos[1] - rect.height / 2);
+                    maxX = Math.max(maxX, pos[0] + rect.width / 2);
+                    maxY = Math.max(maxY, pos[1] + rect.height / 2);
+                    maxWidth = Math.max(maxWidth, rect.width);
+                    maxHeight = Math.max(maxHeight, rect.height);
+                }
+
+                // Округляем размеры до ближайшего целого числа
+                maxWidth = Math.round(maxWidth);
+                maxHeight = Math.round(maxHeight);
+                minX = Math.round(minX);
+                minY = Math.round(minY);
+                maxX = Math.round(maxX);
+                maxY = Math.round(maxY);
+
+                // Создаем новую композицию с уникальным именем и максимальными размерами слоя
+                var newComp = project.items.addComp(layer.name + "_precomp", maxX - minX, maxY - minY, comp.pixelAspect, comp.duration, comp.frameRate);
+                
+                // Копируем слой в новую композицию
+                layer.copyToComp(newComp);
 
                 // Вычисляем новое положение слоя в прекомпозиции, чтобы он был в центре
-                var newPosX = precomp.width / 2;
-                var newPosY = precomp.height / 2;
+                var newPosX = (maxX - minX) / 2;
+                var newPosY = (maxY - minY) / 2;
 
                 // Устанавливаем новое положение слоя
-                precomp.layer(1).position.setValue([newPosX, newPosY]);
-                // Устанавливаем прекомпозицию на исходное место слоя
-                precomp.position.setValue(originalPos);
+                for (var t = layer.inPoint; t <= layer.outPoint; t += comp.frameDuration) {
+                    newComp.layer(1).position.setValueAtTime(t, [newPosX, newPosY]);
+                }
+                                // Заменяем исходный слой на новую композицию
+                var newLayer = comp.layers.add(newComp);
+                newLayer.moveBefore(layer);
+                newLayer.position.setValue(originalPos);
+                layer.remove();
             }
         }
     }
