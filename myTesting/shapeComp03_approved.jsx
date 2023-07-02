72c69f0512d1619daa99acd22c7f6460f9d53580
motionthunder <motionthunder@gmail.com>
2023-07-02 17:06:21 +0300

    _fix

---
+++ myTesting/shapeComp03_approved.jsx
@@ -0,0 +1,76 @@
+// Создаем Undo Group для возможности отмены действий скрипта
+app.beginUndoGroup("Create Precomps for Shape Layers");
+
+// Получаем активный проект
+var project = app.project;
+
+// Проверяем, есть ли активный проект
+if (!project) {
+    alert("No active project");
+} else {
+    // Получаем активную композицию
+    var comp = project.activeItem;
+
+    // Проверяем, есть ли активная композиция
+    if (!comp || !(comp instanceof CompItem)) {
+        alert("No active composition");
+    } else {
+        // Проходим по всем слоям в композиции
+        for (var i = 1; i <= comp.numLayers; i++) {
+            var layer = comp.layer(i);
+
+            // Проверяем, является ли слой шейповым
+            if (layer.matchName === "ADBE Vector Layer") {
+                // Сохраняем исходное положение слоя
+                var originalPos = layer.position.value;
+
+                // Вычисляем максимальные и минимальные координаты слоя на протяжении всего времени анимации
+                var minX = Infinity;
+                var minY = Infinity;
+                var maxX = -Infinity;
+                var maxY = -Infinity;
+                for (var t = layer.inPoint; t <= layer.outPoint; t += comp.frameDuration) {
+                    var rect = layer.sourceRectAtTime(t, false);
+                    var pos = layer.position.valueAtTime(t, false);
+                    minX = Math.min(minX, pos[0] - rect.width / 2);
+                    minY = Math.min(minY, pos[1] - rect.height / 2);
+                    maxX = Math.max(maxX, pos[0] + rect.width / 2);
+                    maxY = Math.max(maxY, pos[1] + rect.height / 2);
+                }
+
+                // Округляем координаты до ближайшего целого числа
+                minX = Math.round(minX);
+                minY = Math.round(minY);
+                maxX = Math.round(maxX);
+                maxY = Math.round(maxY);
+
+                // Вычисляем виртуальные размеры и центр прекомпозиции
+                var precompWidth = maxX - minX;
+                var precompHeight = maxY - minY;
+                var precompCenter = [(minX + maxX) / 2, (minY + maxY) / 2];
+
+                // Создаем новую композицию с уникальным именем и вычисленными размерами
+                var newComp = project.items.addComp(layer.name + "_precomp", precompWidth, precompHeight, comp.pixelAspect, comp.duration, comp.frameRate);
+
+                // Копируем слой в новую композицию
+                layer.copyToComp(newComp);
+
+                // Вычисляем новое положение слоя в прекомпозиции, чтобы он был в центре
+                var newLayerInComp = newComp.layer(1);
+                for (var t = newLayerInComp.inPoint; t <= newLayerInComp.outPoint; t += comp.frameDuration) {
+                    var originalPosAtTime = layer.position.valueAtTime(t, false);
+                    newLayerInComp.position.setValueAtTime(t, [originalPosAtTime[0] - minX, originalPosAtTime[1] - minY]);
+                }
+
+                // Заменяем исходный слой на новую композицию
+                var newLayer = comp.layers.add(newComp);
+                newLayer.moveBefore(layer);
+                newLayer.position.setValue(precompCenter);
+                layer.remove();
+            }
+        }
+    }
+}
+
+// Закрываем Undo Group
+app.endUndoGroup();
